ed1e87ecbfdd4364fdc5570394c89f37a7b76ae5
maxslavin17 <maxdslavin@gmail.com>
2022-04-22 11:55:04 -0500

    Update CreateToDo.js

--- hw2/csc-360-assignment2/src/CreateToDo.js
+++ hw2/csc-360-assignment2/src/CreateToDo.js
@@ -3,20 +3,18 @@ import React, {useState} from "react";
 export default function CreateToDo( {user, toDos, setToDos} ) {
   const [title, setTitle] = useState("")
   const [desc, setDesc] = useState("")
-  const [date, setDate] = useState("")
   
   function handleTitle (evt) { setTitle(evt.target.value); }
   function handleDesc (evt) { setDesc(evt.target.value); }
-  function handleCreate (evt) {
-    setDate(Date.now());
-    const newToDo = { title, desc, date };
+  function handleCreate () {
+    const newToDo = { title, desc, createdDate: Date.now() };
     setToDos( [newToDo, ...toDos] );
   }
 
   return (
     <>
     <h2>Create a Task</h2>
-    <form onSubmit={ (e) => {e.preventDefault(); handleCreate(e);} }>
+    <form onSubmit={ (e) => {e.preventDefault(); handleCreate()} }>
       <div>
         Author: <b>{user}</b>
       </div>
